8943a80d4274d5c16c35fc52aec6967c6093b68d
Helio Schultz <srlhyo@gmail.com>
2024-07-14 15:20:09 +0100

    basic form template

--- public/index.php
+++ public/index.php
@@ -9,25 +9,26 @@
 	<!-- register form START -->
 	<form action="">
 		<label for="name">Name:</label>
-		<input id="name" type="text" name="name">
+		<input id="name" type="text" name="name" placeholder="insert your name" aria-invalid="true">
 
 		<label for="email">Email: </label>
-		<input id="email" type="text" email="email">
-		
+		<input id="email" type="email" email="email" placeholder="insert your email">
 		
 		<fieldset>
 			<legend>Type of User</legend>
 
-			<input id="admin" type="checkbox" name="admin" value="admin">
+			<input id="admin" type="radio" name="admin" value="admin">
 			<label for="admin">Admin</label>
 
-			<input id="agent" type="checkbox" nme="agent" value="agent">
+			<input id="agent" type="radio" nme="agent" value="agent">
 			<label for="agent">Agent</label>
 
 		</fieldset>
 
 		<label for="password">Password: </label>
-		<input id="password" type="password" name="password">
+		<input id="password" type="password" name="password" placeholder="insert your password" aria-describedby="password-hint" aria-invalid="true">
+		<div id="password-hit">Please enter minimum 8 characters long</div>
+ 
 
 		<button type="submit">Register</button>
 	</form>
